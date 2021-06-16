4c6315d97b38632a7e16abb6461436a7c51ae0b7
saipratyushreddy <saipratyushreddy@gmail.com>
2021-06-16 11:03:50 +0530

    commit

--- Routes/stock_update.js
+++ Routes/stock_update.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/g_store',{
 router.post('/',async(req,res)=>{
     console.log(req.body._id)
     stock.findOneAndUpdate({_id:req.body._id},
-        {"$set":{image:req.body.image,category:req.body.category,title:req.body.title,quantity:req.body.quantity,units:req.body.units,price:req.body.price,description:req.body.description}},
+        {"$set":{image:req.body.image,category:req.body.category,title:req.body.title,quantity:req.body.quantity,totalStock:req.body.totalStock,units:req.body.units,price:req.body.price,description:req.body.description}},
         (err,response)=>{
         if(err) throw err;
         else{
